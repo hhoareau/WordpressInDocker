be37914f6afa8c87d7681a076166cbdd4de1f94f
Hoareau <hhoareau@gmail.com>
2018-11-06 12:31:32 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ sudo curl -sSL get.docker.com | sh
 sudo apt-get install -y python-pip
 sudo pip install docker-compose
 
+rm docker-compose.yml
 wget https://raw.githubusercontent.com/hhoareau/WordpressInDocker/master/docker-compose.yml
 docker-compose up -d
