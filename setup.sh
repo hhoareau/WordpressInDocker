7c53095ff5b9491910cb33c148f08fd6e493b33f
Hoareau <hhoareau@gmail.com>
2018-12-06 16:34:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,14 @@ sudo apt-get install -y python-pip
 sudo pip install setuptools
 sudo pip install docker-compose
 
+cd /home/pi
+rm upload.ini
+echo "file_uploads = On" > upload.ini
+echo "memory_limit = 64M" >> upload.ini
+echo "upload_max_filesize = 64M" >> upload.ini
+echo "post_max_size = 64M" >> upload.ini
+echo "max_execution_time = 600" >> upload.ini
+
 rm docker-compose.yml
 wget https://raw.githubusercontent.com/hhoareau/WordpressInDocker/master/docker-compose.yml
 docker-compose up -d
