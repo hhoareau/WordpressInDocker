db60c580f4a2bd86eb2d54197267f80f169df0e9
Hoareau <hhoareau@gmail.com>
2018-12-07 06:55:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,16 @@ sudo pip install setuptools
 sudo pip install docker-compose
 
 cd /home/pi
-rm upload.ini
-echo "file_uploads = On" > upload.ini
-echo "memory_limit = 64M" >> upload.ini
-echo "upload_max_filesize = 64M" >> upload.ini
-echo "post_max_size = 64M" >> upload.ini
-echo "max_execution_time = 600" >> upload.ini
+rm uploads.ini
+echo "file_uploads = On" > uploads.ini
+echo "memory_limit = 64M" >> uploads.ini
+echo "upload_max_filesize = 64M" >> uploads.ini
+echo "post_max_size = 64M" >> uploads.ini
+echo "max_execution_time = 600" >> uploads.ini
 
-rm docker-compose.yml
+sudo docker rm -f f80_wp_container
+sudo docker rm -f f80_db_container
+
+rm docker-compose.yml -y
 wget https://raw.githubusercontent.com/hhoareau/WordpressInDocker/master/docker-compose.yml
 sudo docker-compose up -d
